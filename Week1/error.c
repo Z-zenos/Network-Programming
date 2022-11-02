08e491b1b404c2fd8bf13849e56e97e170baee96
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-11-02 18:40:30 +0700

    Adding error input valid and server error

--- Week1/error.c
+++ Week1/error.c
@@ -2,7 +2,7 @@
 #include "log.h"
 #include "error.h"
 
-#define NUM_OF_ERRORS 12
+#define NUM_OF_ERRORS 14
 
 struct ErrorMessage {
   ErrorCode errorCode;
@@ -16,12 +16,14 @@ struct ErrorMessage errors[NUM_OF_ERRORS] = {
 {ERR_ACCOUNT_NOT_FOUND, "Account not found."},
 {ERR_ACTIVATION_CODE_INCORRECT, "Activation code incorrect."},
 {ERR_INPUT_EMPTY, "Input empty."},
+{ERR_INPUT_INVALID, "Input invalid."},
 {ERR_OPEN_FILE, "Can't open file."},
 {ERR_OPTION, "Bad option."},
 {ERR_MEMORY_FULL, "Memory full."},
 {ERR_NON_LOG_IN, "You are not logged in."},
 {ERR_PASSWORD_INCORRECT, "Password incorrect."},
 {ERR_USERNAME_INCORRECT, "Username incorrect."},
+{ERR_SERVER_ERROR, "Internal server error."},
 };
 
 void err_error(ErrorCode err) {
