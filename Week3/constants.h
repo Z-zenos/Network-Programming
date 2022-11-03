4b33e4dbf7248159760eb3f8da0ed9ae3b797552
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-11-03 22:01:34 +0700

    Add constanst MAX WRONG PW AND MAX WRONG CODE

--- Week3/constants.h
+++ Week3/constants.h
@@ -2,6 +2,8 @@
 #define MAX_USERNAME 50
 #define MAX_PASSWORD 50
 #define MAX_HOMEPAGE 254
+#define MAX_WRONG_PASSWORD 4 // 3 time + 1 first input
+#define MAX_WRONG_CODE 5     // 4 time + 1 first input
 #define BUFFER 1000
 #define ACTIVATION_CODE "20194705"
 #define USER_FILE "account.txt"
