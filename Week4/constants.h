830392d0b71ae44293ee0f2afa793884a9b38dfc
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-11-14 00:11:45 +0700

    Add max request, response, activate, http method length

--- Week4/constants.h
+++ Week4/constants.h
@@ -1,10 +1,14 @@
 
 #define MAX_USERNAME 50
 #define MAX_PASSWORD 50
+#define MAX_REQUEST_LENGTH 4096
+#define MAX_RESPONSE_LENGTH 4096
+#define MAX_ACTIVATE_CODE_LENGTH 50
 #define MAX_HOMEPAGE 254
 #define MAX_WRONG_PASSWORD 4 // 3 time + 1 first input
 #define MAX_WRONG_CODE 5     // 4 time + 1 first input
 #define MAX_MESSAGE 255
+#define MAX_HTTP_METHOD 10
 #define BUFFER 1000
 #define ACTIVATION_CODE "20194705"
 #define USER_FILE "account.txt"
