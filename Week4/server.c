304ccb1bd7db82e698e493ed602ab6b210056f0b
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-11-13 11:22:06 +0700

    Add verifyAccount function, change mechanism return response, add update, login account, first step with route

--- Week4/server.c
+++ Week4/server.c
@@ -7,11 +7,27 @@
 #include "error.h"
 #include "http.h"
 #include "linkedlist.h"
-#include "log.h"
 #include "network.h"
 #include "utils.h"
 
 XOR_LL acc_ll = XOR_LL_INITIALISER;
+
+int verifyAccount(char *request, char *response) {
+  sscanf(request, "/verify/%s", request);
+  XOR_LL_ITERATOR itr = XOR_LL_ITERATOR_INITIALISER;
+
+  XOR_LL_LOOP_HTT_RST(&acc_ll, &itr) {
+    Account *acc = (Account*)itr.node_data.ptr;
+    if(strcmp(acc->username, request) == 0) {
+      // responsify
+      sprintf(response, "200 success %s %d %d", acc->username, acc->num_time_wrong_code, acc->num_time_wrong_password);
+      return SUCCESS;
+    }
+  }
+
+  strcpy(response, "404 fail");
+  return FAIL;
+}
 
 int getAccount(char *request, char *response) {
   sscanf(request, "/accounts/%s", request);
@@ -21,7 +37,7 @@ int getAccount(char *request, char *response) {
     Account *acc = (Account*)itr.node_data.ptr;
     if(strcmp(acc->username, request) == 0) {
       // responsify
-      sprintf(response, "%s %s %d %d %d %s", acc->username, acc->password, acc->status, acc->num_time_wrong_code, acc->num_time_wrong_password, acc->homepage);
+      sprintf(response, "200 success %s %s %d %d %d %s", acc->username, acc->password, acc->status, acc->num_time_wrong_code, acc->num_time_wrong_password, acc->homepage);
       return SUCCESS;
     }
   }
@@ -43,30 +59,95 @@ int createAccount(char *request, char *response) {
   return SUCCESS;
 }
 
+int updateAccount(char *request, char *response) {
+  Account *update_account = (Account *)malloc(sizeof(*update_account));
+  sscanf(
+    request,
+    "/accounts?data: %s %d %d %d",
+    update_account->username, &update_account->status, &update_account->num_time_wrong_code, &update_account->num_time_wrong_password
+  );
+
+  XOR_LL_ITERATOR itr = XOR_LL_ITERATOR_INITIALISER;
+  XOR_LL_LOOP_HTT_RST(&acc_ll, &itr) {
+    Account *acc = (Account*)itr.node_data.ptr;
+    if(strcmp(acc->username, update_account->username) == 0) {
+      // responsify
+      acc->status = update_account->status;
+      acc->num_time_wrong_code = update_account->num_time_wrong_code;
+      acc->num_time_wrong_password = update_account->num_time_wrong_password;
+
+      save_data(acc_ll);
+      sprintf(response, "200 success Login successfully");
+      return SUCCESS;
+    }
+  }
+
+  return FAIL;
+}
+
+int login(char *request, char *response) {
+  char username[MAX_USERNAME];
+  char password[MAX_PASSWORD];
+  sscanf(request, "/authen?data: %s %s", username, password);
+  XOR_LL_ITERATOR itr = XOR_LL_ITERATOR_INITIALISER;
+
+  XOR_LL_LOOP_HTT_RST(&acc_ll, &itr) {
+    Account *acc = (Account*)itr.node_data.ptr;
+    if(strcmp(acc->username, username) == 0 && strcmp(acc->password, password) == 0) {
+      // Check status of account(if account blocked/not activated -> return main menu)
+      if(acc->status == 0) {
+        sprintf(response, "%s", "403 fail Account blocked");
+        return FAIL;
+      }
+
+      if(acc->status == 2) {
+        sprintf(response, "%s", "403 fail Account non active");
+        return FAIL;
+      }
+
+      acc->status = -1;
+      acc->num_time_wrong_code = 0;
+      acc->num_time_wrong_password = 0;
+      save_data(acc_ll);
+      sprintf(response, "202 success %s %s %d %d %d %s", acc->username, acc->password, acc->status, acc->num_time_wrong_code, acc->num_time_wrong_password, acc->homepage);
+      return SUCCESS;
+    }
+    else if(strcmp(acc->username, username) == 0) {
+      ++acc->num_time_wrong_password;
+      if(acc->num_time_wrong_password == MAX_WRONG_PASSWORD) {
+        acc->status = 0;
+      }
+      save_data(acc_ll);
+      sprintf(response, "401 fail %d %d", acc->num_time_wrong_code, acc->num_time_wrong_password);
+      return FAIL;
+    }
+  }
+  return FAIL;
+}
+
 void server_listen() {
   char method[10], request[BUFFER], response[BUFFER];
-
   for(;;) {
     // Clear method, request, response
-    strcpy(method, "");
-    strcpy(request, "");
-    strcpy(response, "");
+    http_clear(method, request, response);
 
-    get_request(method, request, response);
+    get_request(method, request);
 
     if(strcmp(method, "GET") == 0) {
-      if(str_start_with(request, "/accounts/")) getAccount(request, response);
+      if(str_start_with(request, "/verify/")) verifyAccount(request, response);
+      else if(str_start_with(request, "/accounts/")) getAccount(request, response);
 
     }
     else if(strcmp(method, "POST") == 0) {
       if(str_start_with(request, "/accounts?data:")) createAccount(request, response);
+      if(str_start_with(request, "/authen?data:")) login(request, response);
 
     }
     else if(strcmp(method, "PATCH") == 0) {
 
     }
 
-    send_response(method, request, response);
+    send_response(response);
   }
 }
 
